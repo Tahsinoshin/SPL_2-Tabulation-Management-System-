e3ce1a26ce2db83063957c607dd41f93fde2db9a
Tahsinoshin <48786946+Tahsinoshin@users.noreply.github.com>
2022-06-07 15:28:14 +0600

    Add files via upload

--- SPL_2/vishweb/sidebar.php
+++ SPL_2/vishweb/sidebar.php
@@ -4,22 +4,39 @@
             </div>
             <ul class="list-unstyled components">
 			<li  class="active">
-                    <a href="#" class="dashboard"><i class="material-icons">dashboard</i><span>Dashboard</span></a>
+                    <a href="https://www.google.com/" class="dashboard"><i class="material-icons">dashboard</i><span>Dashboard</span></a>
                 </li>
 		
                 <li class="">
-                    <a href="#homeSubmenu1">
-					<i class="material-icons">aspect_ratio</i><span>Marks Entry</span></a>
+                    <a href="../Faculty_action/download_by_faculty_ui.php">
+					<i class="material-icons">aspect_ratio</i><span>Download Sheet</span></a>
+                </li>
+
+                <li  class="">
+                    <a href="../Faculty_action/upload_by_faculty_ui.php"><i class="material-icons">library_books</i><span>Upload Marks</span></a>
+                </li>
+
+                <li  class="">
+                    <a href="printForm/print-request-form.php"><i class="material-icons">library_books</i><span>Download Sheet to Edit</span></a>
+                </li>
+
+                <li  class="">
+                    <a href="printForm/print-request-form.php"><i class="material-icons">library_books</i><span>Upload Edited Sheet</span></a>
                 </li>
                 
                 <li  class="">
-                    <a href="#"><i class="material-icons">library_books</i><span>Request Tabulation Sheet</span></a>
+                    <a href="printForm/print-request-form.php"><i class="material-icons">library_books</i><span>Print Request</span></a>
                 </li>
 	  
 				
 				<li class="">
                     <a href="#pageSubmenu5">
 					<i class="material-icons">border_color</i><span>Mark as Final</span></a>
+                </li>
+
+                <li class="">
+                    <a href="../signup_login/updates/faculty-pass-update.php">
+					<i class="material-icons">aspect_ratio</i><span>Update Password</span></a>
                 </li>
                 
           
